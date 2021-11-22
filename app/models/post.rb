4602e51abbc1effeb16c7a816db054a76d0229e4
love.cosma <williamsmatthew333@gmail.com>
2021-11-22 13:17:25 -0500

    commit tree 7d85817dac668c07b654493660349ab6975e33c6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: {:minimum => 250}
+    validates :summary, length: {:maximum => 250}
+    validates :category, inclusion: %w(Fiction Non-Fiction)
+    validate :clickbait
+
+
+
+    def clickbait
+        unless %r(\bWon't Believe|Secret|Top \d+|Guess).match?(self.title)
+            errors.add(:title, "This title is not clickbait-y enough")
+        end
+    end
+
+
+end 
